17362d868ae290219fd52e44bcbe9f05c2cbab07
Matt Stratton <matt.stratton@gmail.com>
2014-01-23 20:54:01 -0600

    Update to version 1.0.0

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'mstratton@10thmagnitude.com'
 license          'All rights reserved'
 description      'Installs/Configures xm-jenkins'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '1.0.0'
 
 depends 'jenkins', '~> 2.0.0'
