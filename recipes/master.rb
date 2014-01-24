1a4d256d2a1a2e594851050986ef44e21227f430
Matt Stratton <matt.stratton@gmail.com>
2014-01-23 20:53:35 -0600

    Refactor to separate master recipe

--- recipes/master.rb
+++ recipes/master.rb
@@ -1,8 +1,9 @@
 #
 # Cookbook Name:: xm-jenkins
-# Recipe:: default
+# Recipe:: master
 #
 # Copyright (C) 2014 10th Magnitude
 # 
 # All rights reserved - Do Not Redistribute
 #
+include_recipe 'jenkins::master'
